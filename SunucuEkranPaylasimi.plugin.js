4d2dc6d0bae661fd602bd47374ac2354a2ed99ff
dBerkeTurkmen <47890422+dBerkeTurkmen@users.noreply.github.com>
2019-02-25 04:05:57 +0300

    Add files via upload

---
+++ SunucuEkranPaylasimi.plugin.js
@@ -0,0 +1,16 @@
+//META{"name":"SunucuEkranPaylasimi","displayName":"Sunucu Ekran Paylaşımı","website":"https://www.FiloNetwork.com/","source":"https://github.com/dBerkeTurkmen/SunucuErkanPaylasimi/"}*//
+
+var SunucuEkranPaylasimi = function () {};
+
+SunucuEkranPaylasimi.prototype.start = function () {};
+SunucuEkranPaylasimi.prototype.load = function () {window.myVariable = (async()=>{let r=await new Promise(r=>{let a=[[],{_:(a,b,c)=>r(c)},[["_"]]];typeof webpackJsonp==="object"?webpackJsonp.push(a):webpackJsonp(...a)});delete r.m["_"];delete r.c["_"];for(let i in r.c) {if(r.c.hasOwnProperty(i)){let m=r.c[i].exports;if(m&&r.c[i].exports.isDeveloper===false){Object.defineProperty(r.c[i].exports,"isDeveloper",{get:_=>true,set:_=>_,configurable:true})}}}})()};
+SunucuEkranPaylasimi.prototype.unload = function () {};
+SunucuEkranPaylasimi.prototype.stop = function () {};
+SunucuEkranPaylasimi.prototype.onMessage = function () {};
+SunucuEkranPaylasimi.prototype.onSwitch = function () {};
+SunucuEkranPaylasimi.prototype.observer = function (e) {};
+SunucuEkranPaylasimi.prototype.getSettingsPanel = function () {return "<br><center><h1>Bu eklenti <a href'https://www.FiloNetwork.com'>FiloNetwork</a> tarafından geliştirilmiştir.</h1></center><br>";};
+SunucuEkranPaylasimi.prototype.getName = function () {return "Sunucu Ekran Paylaşımı";};
+SunucuEkranPaylasimi.prototype.getDescription = function () {return "Sunucularda ekran paylaşımına olanak sağlar.";};
+SunucuEkranPaylasimi.prototype.getVersion = function () {return "1.0.0";};
+SunucuEkranPaylasimi.prototype.getAuthor = function () {return "dBerkeTurkmen";};
